d9880b52ec738f3d9f935f88c27b4181cea430c7
Ezequiel <Ezequiel@DESKTOP-HO78NQT>
2024-04-13 14:35:44 -0300

    Update

---
+++ tp1_AvilesEzequielGonzalo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Mexico";
+		int edad = 33;
+		float alturaEdificio= 32.3f;
+		float precio = 59.99f;
+		long numeroTelefono = 38841523577l;
+		double calculoCoseno = 0.999966569231;
+		
+		System.out.println(nombrePais);
+		System.out.println(edad);
+		System.out.println(alturaEdificio);
+		System.out.println(precio);
+		System.out.println(numeroTelefono);
+		System.out.println(calculoCoseno);
+	}
+
+}
